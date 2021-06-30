e9adbd4768b14cdd6a813f69138a907cb180363f
Dmitry Ishkov <dimanrussian@gmail.com>
2021-06-30 21:54:33 +0400

    comments

--- ractor_server.rb
+++ ractor_server.rb
@@ -4,28 +4,40 @@ require_relative 'request_parser'
 class RactorServer
   PORT = ENV.fetch('PORT', 3000)
   BIND = ENV.fetch('BIND', '127.0.0.1').freeze
-  READ_QUEUE = ENV.fetch('QUEUE', 12).to_i
+  SOCKET_READ_BACKLOG = ENV.fetch('TCP_BACKLOG', 12).to_i
   WORKERS_COUNT = ENV.fetch('WORKERS', 4).to_i
 
   attr_accessor :app
 
+  # app: Rack app
   def initialize(app)
     self.app = app
   end
 
   def start
-    queue = Ractor.new do
+    # the dispatcher is going to be used to
+    # fairly dispatch incoming requests,
+    # we pass the queue into workers
+    # and the first free worker gets
+    # the yielded request
+    dispatcher = Ractor.new do
       loop do
         conn = Ractor.recv
         Ractor.yield(conn, move: true)
       end
     end
 
+    # workers determine concurrency
     WORKERS_COUNT.times.map do
-      Ractor.new(queue, self) do |queue, server|
+      # we need to pass the dispatcher and the server so they are available
+      # inside Ractor
+      Ractor.new(dispatcher, self) do |dispatcher, server|
         loop do
-          conn = queue.take
+          # this method blocks until the dispatcher yields a connection
+          conn = dispatcher.take
           request = RequestParser.new(conn).parse
+          # in a real app there would be a whole lot more information
+          # about the request, but we are gonna keep it simple
           status, headers, body = server.app.call(
              'REQUEST_METHOD' => request.method,
              'PATH_INFO' => request.path,
@@ -38,11 +50,14 @@ class RactorServer
       end
     end
 
-    listener = Ractor.new(queue) do |queue|
+    # the listener is going to accept new connections
+    # and pass them onto the dispatcher,
+    # we make it a separate Ractor so it responds quicker
+    listener = Ractor.new(dispatcher) do |queue|
       socket = Socket.new(:INET, :STREAM)
       socket.setsockopt(Socket::SOL_SOCKET, Socket::SO_REUSEADDR, true)
       socket.bind(Addrinfo.tcp(BIND, PORT))
-      socket.listen(READ_QUEUE)
+      socket.listen(SOCKET_READ_BACKLOG)
       loop do
         conn, _addr_info = socket.accept
         queue.send(conn, move: true)
