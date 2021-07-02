6df072e3549ba13ea162ea9139a5551d93ebd043
Dmitry Ishkov <dimanrussian@gmail.com>
2021-07-02 21:21:36 +0400

    add a comment that WebRequestApp doesn't work with ractors

--- apps/web_request_app.rb
+++ apps/web_request_app.rb
@@ -2,6 +2,9 @@ require 'open-uri'
 
 class WebRequestApp
   def call(env)
+    # unfortunately, URI is not going to work with ractors yet
+    # https://bugs.ruby-lang.org/issues/17592
+
     body = URI.open('http://example.com').read
     [200, { "Content-Type" => "text/html" }, [body]]
   end
